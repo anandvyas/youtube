6398a69e79af8aebeb584f4f365ef4491a36a7f8
apple <apple@apples-MacBook-Air.local>
2018-10-20 18:34:30 +0530

    Multiple instance changes

--- oneinstance/run.sh
+++ oneinstance/run.sh
@@ -2,6 +2,7 @@
 
 mozillaProfile="hhytcn5z"
 currentDir=$(pwd)
+processId="$(date +%s)"
 
 while true; do
 
@@ -23,6 +24,17 @@ while true; do
     videoId=${entity[0]}
     videoMinutes=${entity[1]}
 
+    if [ ! $videoMinutes -gt 0 ]; then
+        echo "Video minutes not readable"
+        $videoMinutes = 5
+    fi
+
+     # video duration
+    totalDuration=$(( $videoMinutes * 60 ))
+    halfDuration=$(( $totalDuration / 2 ))
+    randDuration=$(((RANDOM%$halfDuration)))
+    finalDuration=$(( $halfDuration + $randDuration ))
+    
     #browser array
     #browser=('chromium-browser' 'firefox')
     browser=('chromium-browser')
@@ -31,51 +43,44 @@ while true; do
     randBrowser=${browser[$browserRandNumber]}
 
     # Implement TOR Proxy
-    command="docker run --name youtube-proxy --restart=always -it -p 8118:8118 -d dperson/torproxy"
+    command="docker run --name youtube-$processId-proxy --restart=always -it -p 8118 -d dperson/torproxy"
     $command
 
     # Get Proxy IP
-    randProxy=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' youtube-proxy)
-    randProxy="$randProxy"
-
+    proxyIp=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' youtube-$processId-proxy)
+    proxyPort="$(docker inspect --format='{{(index (index .NetworkSettings.Ports "8118/tcp") 0).HostPort}}' youtube-$processId-proxy)"
+    
     # Copy master profile to temp
-    command="rm -rf $currentDir/temp"
+    command="rm -rf $currentDir/$processId"
     $command
-
-    command="cp -r $currentDir/mozilla $currentDir/temp"
+    command="cp -r $currentDir/mozilla $currentDir/$processId"
     $command
 
     # Make proxy entry in profile
-    filepath="$currentDir/temp/firefox/$mozillaProfile.default/prefs.js"
+    filepath="$currentDir/$processId/firefox/$mozillaProfile.default/prefs.js"
 
-    echo "user_pref(\"network.proxy.ssl\", \"$randProxy\");" >> $filepath
-    echo "user_pref(\"network.proxy.ssl_port\", 8118);" >> $filepath
-    echo "user_pref(\"network.proxy.http\", \"$randProxy\");" >> $filepath
-    echo "user_pref(\"network.proxy.http_port\", 8118);" >> $filepath
+    echo "user_pref(\"network.proxy.ssl\", \"$proxyIp\");" >> $filepath
+    echo "user_pref(\"network.proxy.ssl_port\", $proxyPort);" >> $filepath
+    echo "user_pref(\"network.proxy.http\", \"$proxyIp\");" >> $filepath
+    echo "user_pref(\"network.proxy.http_port\", $proxyPort);" >> $filepath
     echo "user_pref(\"network.proxy.type\", 1);" >> $filepath
     echo "user_pref(\"app.update.enabled\", false); " >> $filepath
-
-    # video duration
-    totalDuration=$(( $videoMinutes * 60 ))
-    halfDuration=$(( $totalDuration / 2 ))
-    randDuration=$(((RANDOM%$halfDuration)))
-    finalDuration=$(( $halfDuration + $randDuration ))
+    
+    command="docker run --name=youtube-$processId-player --link=youtube-$processId-proxy -d -p 6901 -v $(pwd)/$processId:/headless/.mozilla -e VNC_RESOLUTION=800x600 -e VNC_PW="1234"  consol/ubuntu-xfce-vnc firefox --profile=$mozillaProfile --setDefaultBrowser https://www.youtube.com/watch?v=$videoId "
+    $command
 
     echo "Video https://www.youtube.com/watch?v=$videoId" 
     echo "Browser: $randBrowser"
-    echo "Proxy: $randProxy:8118"
+    echo "Proxy: $proxyIp:$proxyPort"
+    echo "Browser port $(docker inspect --format='{{(index (index .NetworkSettings.Ports "6901/tcp") 0).HostPort}}' youtube-$processId-player)"
     echo "Duration: $totalDuration finalDuration: $finalDuration"
     echo "Start time: $(date +%d/%m/%Y-%H:%M:%S)"
     
-    command="docker run --name=youtube-player --link=youtube-proxy -d -p 6901:6901 -v $(pwd)/temp:/headless/.mozilla -e VNC_RESOLUTION=800x600 -e VNC_PW="1234"  consol/ubuntu-xfce-vnc firefox --profile=$mozillaProfile --setDefaultBrowser https://www.youtube.com/watch?v=$videoId "
-    $command
-
     sleep $finalDuration
 
     ## Remove docker running images
-    command="docker kill $(docker ps -a --format '{{.Names}}' | grep -G "^youtube-*")"
+    command="docker kill $(docker ps -a --format '{{.Names}}' | grep -G "^youtube-$processId-*")"
     $command
-    command="docker rm $(docker ps -a --format '{{.Names}}' | grep -G "^youtube-*")"
+    command="docker rm $(docker ps -a --format '{{.Names}}' | grep -G "^youtube-$processId-*")"
     $command
-
 done
